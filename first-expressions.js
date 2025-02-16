e383d8c6706f6bb56fd066fed6b2341481dd8403
Biniyam-Biyadge <biniyambiyadge@gmail.com>
2025-02-16 14:13:36 -0800

    new

---
+++ first-expressions.js
@@ -0,0 +1,9 @@
+console.log("i am biniyam");
+console.log(3 + 2);
+var c=12;
+var f=c*1.8+32;
+console.log(f);
+let string = "She said, \"Hello!\"";
+console.log(string);
+let stringg="abyalew \"hello\"";
+console.log(stringg);
